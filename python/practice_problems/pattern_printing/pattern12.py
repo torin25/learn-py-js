ec6c0efe0cb957cc666aed55badd494f59793df4
torin25 <tejasmcodes@gmail.com>
2026-02-22 15:45:09 +0530

    add pattern12

---
+++ python/practice_problems/pattern_printing/pattern12.py
@@ -0,0 +1,13 @@
+n=8
+space = " "
+
+for i in range(1,4):
+    for j in range(1,i+1):
+        print(j,end="")
+    
+    print(space*(n-2*i),end="")
+
+    for k in range(i,0,-1):
+        print(k,end="")
+
+    print()
